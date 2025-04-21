b31f629438d18eba2b230aa237b06f4194a2ff10
Eddie Yeung <eycs0317@gmail.com>
2025-04-21 16:01:07 -0700

    add nextauth

--- src/ui/layout/hero.tsx
+++ src/ui/layout/hero.tsx
@@ -4,6 +4,8 @@ import Heading from '../patterns/heading';
 // data
 import {getHeroHome} from '../../data/static/layout';
 
+import Link from 'next/link';
+
 export default function Header() {
   const hero = getHeroHome();
 
@@ -11,7 +13,9 @@ export default function Header() {
     <div className="flex flex-col bg-gray-800 text-white items-center p-10 justify-center gap-6">
       <Heading level={1} content={hero.heading} />
       <p>{hero.subheading}</p>
+      <Link href='/api/auth/signin'>
       <button className="bg-white text-black py-3 px-5 rounded-md hover:bg-gray-200">{hero.cta}</button>
+      </Link>
     </div>
   );
 }
