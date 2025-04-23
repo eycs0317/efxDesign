b9fea4d4b8977975324ddb95bcad7fa24319c8c2
Eddie Lou <eddie@efx-design.com>
2025-04-23 14:57:36 -0700

    Re-add Sign Out

--- src/data/static/layout.ts
+++ src/data/static/layout.ts
@@ -14,6 +14,7 @@ export function getGlobalNav() {
     { name: 'Services', href: '/services' },
     { name: 'Contact', href: '/contact' },
     { name: 'Sign In', href: '/signIn' },
+    { name: 'Sign Out', href: '/signOut' },
   ];
 }
 
